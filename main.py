b854eb46d85a39a27d3b100ad8acf88c0c8712d9
LauGds <137927575+LauGds@users.noreply.github.com>
2023-07-24 09:55:41 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+data_states = data.state.to_list()
+usa_states = []
+
+while len(usa_states) < 50:
+    answer_state = screen.textinput(title=f"{len(usa_states)}/50 States Correct", prompt="What's another "
+                                                                                         "state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = [state for state in data_states if state not in usa_states]
+        # missing_states = []
+        # for state in data_states:
+        #     if state not in usa_states:
+        #         missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in data_states:
+        correct_states = usa_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# How to get the coordinates with click on screen
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
